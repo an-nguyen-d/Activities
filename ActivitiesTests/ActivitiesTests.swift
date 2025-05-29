955f4d618476f9ce394dec498eaf3eafd3e8d479
An Nguyen <annguyen@knights.ucf.edu>
2025-05-29 11:28:24 -0400

    Initial Commit

---
+++ ActivitiesTests/ActivitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesTests.swift
+//  ActivitiesTests
+//
+//  Created by An Nguyen on 5/29/25.
+//
+
+import Testing
+@testable import Activities
+
+struct ActivitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
